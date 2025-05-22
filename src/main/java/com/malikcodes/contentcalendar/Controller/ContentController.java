97716ee0d1324c51cb95129573ca85296e2c17c1
MalikBogos <malikjandarov@gmail.com>
2025-05-22 05:02:02 +0200

    Started small callendar project (based on a Spring Boot Crash Course)

--- src/main/java/com/malikcodes/contentcalendar/Controller/ContentController.java
+++ src/main/java/com/malikcodes/contentcalendar/Controller/ContentController.java
@@ -1,9 +1,18 @@
 package com.malikcodes.contentcalendar.Controller;
 
+import com.malikcodes.contentcalendar.Repository.ContentCollectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+
+    private final ContentCollectionRepository repository;
+
+    @Autowired
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
 }
